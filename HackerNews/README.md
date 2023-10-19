697ea420eb09341cda5e682ae199e1fbc6523f5b
ROHIT PANDEY <rohit.pandey3825@gmail.com>
2023-10-19 16:53:49 +0530

    commit header

--- HackerNews/README.md
+++ HackerNews/README.md
@@ -1,6 +1,6 @@
 # HackerNews
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
 ## Development server
 
--- HackerNews/src/app/app.module.ts
+++ HackerNews/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
